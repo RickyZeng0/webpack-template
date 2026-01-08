c7d8868e08a32d8b64501d954700ec4641e949b4
Ricky_Zeng <zengchiwai08@gmail.com>
2026-01-08 11:52:32 +0800

    Add command for deployment

--- webpack.common.js
+++ webpack.common.js
@@ -22,3 +22,12 @@ module.exports = {
     ],
   },
 };
+
+/*
+git branch gh-pages
+git checkout gh-pages && git merge main --no-edit
+npm run build
+git add dist -f && git commit -m "Deployment commit"
+git subtree push --prefix dist origin gh-pages
+git checkout main
+*/
